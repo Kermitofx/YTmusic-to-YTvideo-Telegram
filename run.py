c1fd114fd9e01d8f5f7c29b7c65c5693b76a3996
Ignisor <ignis2497@gmail.com>
2019-03-05 12:19:37 +0200

    music.youtube links to youtube links

---
+++ run.py
@@ -0,0 +1,35 @@
+import logging
+
+from telegram import MessageEntity
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+TOKEN = '725075566:AAFSY3aK6DQDcYeejp7fWRIqcG5OO7_wFeo'
+FIND = 'music.youtube.com'
+REPLACE = 'youtube.com'
+
+
+def parse_url(update, context):
+    text = update.message.text
+    if FIND not in text:
+        return
+
+    response = text.replace(FIND, REPLACE)
+    update.message.reply_text(response)
+
+
+if __name__ == '__main__':
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    echo_handler = MessageHandler(
+        Filters.text & (Filters.entity(MessageEntity.URL) | Filters.entity(MessageEntity.TEXT_LINK)),
+        parse_url
+    )
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
+    updater.idle()
+    updater.stop()
